84d1b61a99c1931222f0a5949989d93bb6db7a86
II <ii@IIs-MacBook-Air.local>
2025-12-28 02:33:47 +0300

    fix: update base path for custom domain uchet1.ru

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/firstst-VS/' : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
